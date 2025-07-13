7a94284efce697a6f00f0e6500d8b952455069ed
saMujbo <samuelbarrantes1@gmail.com>
2025-07-13 17:10:16 -0600

    BegingCompanyService

--- src/Services/Company/Candidate.service.Interfaces.ts
+++ src/Services/Company/Candidate.service.Interfaces.ts
@@ -1,5 +1,9 @@
 import { Company } from "src/Entities/Company.entity";
 
 export interface ICompanyService{
-    Addcompany(company:Company): Promise<Company | null>
+    Addcompany(company:Company): Promise<Company[]>
+    DeleteCompanyById(id:number): Promise<void>
+    GetAllCompanies():Promise<Company[]>
+    GetCompanyById(id:number): Promise<Company[]>
+    UpdateCompany(id:number,company:Company): Promise<Company[]>
 }
